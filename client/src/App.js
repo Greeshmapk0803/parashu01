5a1d0c99d5e5790551839a4820dc87ef8e5c37b3
Nikesh <nikeshs129@gmail.com>
2023-10-14 14:17:27 +0530

    [Server] BE integration initiated

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,9 @@ import {
 } from "react-router-dom";
 import { Home, Landing, Login, Signup, Profile, News } from './pages';
 import { newsPages } from './constants';
-
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import Articles from "./components/articles";
 
 
 const theme = createTheme({
@@ -37,10 +39,27 @@ const theme = createTheme({
   },
 });
 
+const API_URL = "http://localhost:3000/api/v1/articles"
 
-
+function getAPIData(){
+  return axios.get(API_URL).then((response) => response.data)
+}
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  console.log('hi')
+
+  useEffect(() => {
+      let mounted = true;
+      getAPIData().then((items) => {
+        if(mounted){
+          setArticles(items);
+          console.log(items)
+        }
+    });
+    return () => (mounted = false);
+  }, []);
+
   return (
     <Router>
       <ThemeProvider theme={theme}>
@@ -53,11 +72,15 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/profile" element={<Profile />} />
+
             {/* //news Endpoints */}
             {newsPages.map((page) => (
               <Route exact path={page.path} element={<News path={page.path} key={page.path} />} />
-            ))}
+              ))}
+              <Route exact path='test' element={<Articles articles={articles} style= {{color: "white"}}/>} />
+
           </Routes>
+
           <Footer />
         </Box>
       </ThemeProvider>
